488c57106e410e39d9ccf178437a1e62aa7b9922
Camel2Go <art.2001@web.de>
2021-12-06 13:46:10 +0100

    edit 2021 - 06

--- 2021/06/aoc.py
+++ 2021/06/aoc.py
@@ -6,17 +6,15 @@ data = [int(x) for x in open(sys.path[0]+"/data").read().split(',')]
 
 # ======== code =======
 
-def run(state, days):
+def run(fish, days):
     for _ in range(days):
-        new = state[0]
-        state = state[1:]
-        state[6] += new
-        state.append(new)
-    return sum(state)
+        new = fish[0]
+        fish = fish[1:]
+        fish[6] += new
+        fish.append(new)
+    return sum(fish)
 
-state = []
-for i in range(9):
-    state.append(data.count(i))
+fish = [data.count(i) for i in range(9)]
 
-print(run(state, 80))
-print(run(state, 256))
+print(run(fish, 80))
+print(run(fish, 256))
