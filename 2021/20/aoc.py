0dd6a8b5c5ac3c84ef03f0e7167fa31d2cdbb5d4
Camel2Go <art.2001@web.de>
2021-12-20 14:01:12 +0100

    crop 2021 - 20

--- 2021/20/aoc.py
+++ 2021/20/aoc.py
@@ -4,37 +4,19 @@
 
 from time import time
 from itertools import product
-from functools import reduce
 import sys
 start = time()
 data = open(sys.path[0] + "/data").read().replace('#', '1').replace('.', '0').split('\n\n')
 
 # ======== code =======
 
-def run(input, outside):
-    out = []
-    tmp = ""
-    (x, y) = (-1, -1)
-    while y != len(input) + 1:
-        n = ""
-        for (j, i) in product([-1, 0, 1], [-1, 0, 1]):
-            if 0 <= x + i < len(input[0]) and 0 <= y + j < len(input): n += input[y + j][x + i]
-            else: n += outside
-        tmp += algorithm[int(n, 2)]
-        x += 1
-        if x == len(input[0]) + 1: 
-            out.append(tmp)
-            tmp = ""
-            x = -1
-            y += 1
-    return out, algorithm[int(9 * outside, 2)]
+def run(data, outside):
+    return [''.join([lookup[int(''.join([data[y + j][x + i] if 0 <= x + i < len(data[0]) and 0 <= y + j < len(data) else outside for (j, i) in product([-1, 0, 1], [-1, 0, 1])]), 2)] for x in range(-1, len(data[0]) + 1)]) for y in range(-1, len(data) + 1)], lookup[int(9 * outside, 2)]
 
-algorithm = data[0]
-input = data[1].split('\n')
-outside = '0'
-print(''.join(run(*run(input, outside))[0]).count('1'))
+lookup, data, outside = data[0], data[1].split('\n'), '0'
+print(''.join(run(*run(data, outside))[0]).count('1'))
 for i in range(50):
-    input, outside = run(input, outside)
-print(''.join(input).count('1'))
+    data, outside = run(data, outside)
+print(''.join(data).count('1'))
 
 print(f"\n===== {time() - start} sec =====")
