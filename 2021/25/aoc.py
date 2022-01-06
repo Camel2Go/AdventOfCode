1b58e6874f2960de891575682264c227f1096a30
Camel2Go <art.2001@web.de>
2022-01-06 01:18:00 +0100

    finish 2021 🎉️

---
+++ 2021/25/aoc.py
@@ -0,0 +1,36 @@
+#!/bin/python
+
+# ======== setup ===========
+
+from time import time
+import sys
+start = time()
+data = open(sys.path[0] + "/data").read().split('\n')
+
+# ======== code =======
+
+def step():
+    global data
+    count = 0
+    for c in ['>', 'v']:
+        move = []
+        for y in range(len(data)):
+            for x in range(len(data[0])):
+                if data[y][x] == c and data[(y + (c == 'v')) % len(data)][(x + (c == '>')) % len(data[0])] == '.':
+                    move.append((x, y))
+        count += len(move)
+        for (x, y) in move:
+            data[y][x] = '.'
+            data[(y + (c == 'v')) % len(data)][(x + (c == '>')) % len(data[0])] = c
+    return count
+
+def run():
+    count = 1
+    while step():
+        count += 1
+    return count
+
+data = [list(line) for line in data]
+print(run())
+
+print(f"\n===== {time() - start} sec =====")
