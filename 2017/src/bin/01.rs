d384c9fb4766704c52b59d19327f66052f0d21dd
Camel2Go <art.2001@web.de>
2022-05-02 21:54:02 +0200

    idk

--- 2017/src/bin/01.rs
+++ 2017/src/bin/01.rs
@@ -1,29 +1,27 @@
+use std::time::Instant;
+use std::iter::zip;
 use std::fs;
-use std::iter;
 
 fn main() {
 
-    let data = fs::read_to_string("data/01").expect("Error while reading data");
-    println!("{}\n", run(data));
+	let data = fs::read_to_string("data/01").expect("Error while reading data");
+
+	let now = Instant::now();
+	
+	println!("{}", run(&data, true));
+	println!("{}", run(&data, false));
+	println!("======= {}s ========", now.elapsed().as_secs_f32());
 }
 
-fn run(data: String) -> u32 {
+fn run(data: &String, part1: bool) -> u32 {
 
-    let mut sum: u32 = 0;
-    let mut data = data.chars();
-    let first = data.next();
-    while let second = data.next() {
-        if second == None {
-            if first == data.nth(0) {
-                sum += first.unwrap().to_digit(10).unwrap();
-            }
-            break;
-        }
-        if first == second {
-            sum += first.unwrap().to_digit(10).unwrap();
-        }
-        let first = second;
-    }
-    // let sum = data.chars().zip(iter::once(data.get(0)).chain(data.chars().skip(1))).filter(|(current, next)| current == next).sum();
-    return sum;
+	let mut comp: Vec<char> = data.chars().collect();
+	
+	if part1 {
+		comp.rotate_right(1);
+	} else {
+		let length = comp.len() / 2;
+		comp.rotate_right(length);
+	}
+	return zip(data.chars(), comp.into_iter()).map(|(current, next)| if current == next {current.to_digit(10).unwrap()} else {0}).sum();
 }
--- 2017/src/bin/02.rs
+++ 2017/src/bin/02.rs
@@ -1,3 +1,27 @@
+use std::time::Instant;
+use std::fs;
+use itertools::Itertools;
+
 fn main() {
-	println!("idk")
+
+	let data = fs::read_to_string("data/02").expect("Error while reading data");
+
+	let now = Instant::now();
+	
+	println!("{}", run(&data, true));
+	println!("{}", run(&data, false));
+	println!("======= {}s ========", now.elapsed().as_secs_f32());
+}
+
+fn run(data: &String, part1: bool) -> u32 {
+
+	let data: Vec<Vec<u32>> = data.split('\n').map(|line| line.split('\t').map(|number| number.parse::<u32>().unwrap()).collect()).collect();
+	
+	if part1 {
+		return data.iter().map(|line| line.iter().max().unwrap() - line.iter().min().unwrap()).sum();
+
+	} else {
+		return data.iter().map(|line| line.iter().permutations(2).map(|tuple| if tuple[0] % tuple[1] == 0 {tuple[0] / tuple[1]} else {0}).sum::<u32>()).sum();
+	}
+
 }
