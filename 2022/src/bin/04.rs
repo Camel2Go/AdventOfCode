1f0b58a435a987bbd249665a28e19338366f1927
Camel2Go <art.2001@web.de>
2022-12-04 11:43:20 +0100

    2022 - 04

---
+++ 2022/src/bin/04.rs
@@ -0,0 +1,32 @@
+use std::time::Instant;
+use std::fs;
+
+fn main() {
+
+	let data = fs::read_to_string("data/04").expect("Error while reading data");
+
+	let time = Instant::now();
+
+	println!("{}", run(&data, false));
+	println!("{}", run(&data, true));
+
+	println!("======= {}s ========", time.elapsed().as_secs_f32());
+}
+
+fn run(data: &String, part2: bool) -> usize {
+
+	let mut count = 0;
+	for pair in data.split("\n").map(|x| x.replace(',', "-").split('-').map(|x| x.parse::<usize>().unwrap()).collect::<Vec<usize>>()) {
+		
+		let first = pair[0]..=pair[1];
+		let second = pair[2]..=pair[3];
+
+		count += if part2 {
+			first.contains(&pair[2]) || first.contains(&pair[3]) || second.contains(&pair[0]) || second.contains(&pair[1])
+		} else {
+			pair[0] <= pair[2] && pair[1] >= pair[3] || pair[0] >= pair[2] && pair[1] <= pair[3]
+		} as usize;
+	}
+
+	return count;
+}
