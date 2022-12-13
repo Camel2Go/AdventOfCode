92a52de9eb3c911e076a5d231f8d78b013b22ada
Camel2Go <art.2001@web.de>
2022-12-13 20:27:59 +0100

    2022 - 11

--- 2022/src/bin/11.rs
+++ 2022/src/bin/11.rs
@@ -1,5 +1,4 @@
-use std::env::VarError;
-use std::num::ParseIntError;
+use std::borrow::BorrowMut;
 use std::time::Instant;
 use std::fs;
 // use regex::Regex;
@@ -16,56 +15,67 @@ fn main() {
 	println!("[\u{23f1}\u{fe0f} ] {}s", time.elapsed().as_secs_f32());
 
 	time = Instant::now();
-// 
+
 	println!("[2\u{fe0f}\u{20e3}\u{fe0f} ] {}", run(&data, true));
 	println!("[\u{23f1}\u{fe0f} ] {}s", time.elapsed().as_secs_f32());
 }
-
+#[derive(Clone, Default, Debug)]
 struct Monkey {
 	items: Vec<usize>,
 	operation: char,
-	operand: Result<usize, ParseIntError>,
+	operand: usize,
 	test: usize,
 	monk_true: usize,
 	monk_false: usize,
+	inspected: usize,
 }
 
 fn run(data: &String, part2: bool) -> usize {
 	
-	let mut monkeys = Vec::<Monkey>::new();
+	let mut monkeys = Vec::new();
 	for mut attrib in data.split("\n\n").map(|m| m.split("\n").skip(1)) {
 		let items = attrib.next().unwrap().chars().skip(18).collect::<String>().split(", ").map(|i| i.parse::<usize>().unwrap()).collect();
-		
 		let tmp = attrib.next().unwrap().chars().skip(23).collect::<Vec<char>>();
 		let operation = tmp[0];
-		let operand = tmp[2..].iter().collect::<String>().parse::<usize>();
-
+		let operand = tmp[2..].iter().collect::<String>().parse::<usize>().unwrap_or_default();
 		let test = attrib.next().unwrap().chars().skip(21).collect::<String>().parse::<usize>().unwrap();
 		let monk_true = attrib.next().unwrap().chars().skip(29).collect::<String>().parse::<usize>().unwrap();
 		let monk_false = attrib.next().unwrap().chars().skip(30).collect::<String>().parse::<usize>().unwrap();
+		let inspected = 0;
 
-
-		let monkey = Monkey {items: items, operation: operation, operand: operand, test: test, monk_true: monk_true, monk_false: monk_false};
-		monkeys.push(monkey)
+		monkeys.push(Monkey {items, operation, operand, test, monk_true, monk_false, inspected})
 	}
-	for mut monkey in monkeys {
-		for mut item in monkey.items {
-			item = match (monkey.operation, monkey.operand) {
-				('+', Ok(x)) => item + x,
-				('*', Ok(x)) => item * x,
-				('+', Error) => 2 * item,
-				('*', Error) => item * item,
-				(_, _) => panic!(),
-			};
-			item /= 3;
-			if (item % monkey.test == 0) {
-				monkeys[monkey.monk_true].items.push(item);
-			} else {
-				monkeys[monkey.monk_false].items.push(item);
+	let modul: usize = monkeys.iter().map(|m| m.test).product();
+
+
+	for _ in 0..if part2 {10000} else {20} {
+		for monkey in 0..monkeys.len() {
+			let monkey = monkeys[monkey].borrow_mut();
+			let mut throw = Vec::new();
+			for mut item in monkey.items.clone() {
+				item = match (monkey.operation, monkey.operand.clone()) {
+					('+', 0) => 2 * item,
+					('*', 0) => item * item,
+					('+', x) => item + x,
+					('*', x) => item * x,
+					(_, _) => panic!(),
+				} % modul;
+				if !part2 {item /= 3};
+				let other = if item % monkey.test == 0 {
+					monkey.monk_true
+				} else {
+					monkey.monk_false
+				};
+				throw.push((other, item));
+				monkey.inspected += 1;
 			}
 			monkey.items.clear();
+			for (other, item) in throw.iter() {
+				monkeys[*other].items.push(*item)
+			};
 		}
 	}
-// };
-	return 0;
+	monkeys.sort_by_key(|m| m.inspected);
+
+	return monkeys.iter().rev().take(2).map(|m| m.inspected).product();
 }
